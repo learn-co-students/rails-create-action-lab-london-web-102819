4fbdeb80e43ebd0bacaddec2c9d06a7091a87b0f
PianoFF <Fangfei1star@gmail.com>
2019-11-26 08:52:12 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+students=Student.create([
+    {
+        first_name: "Joe",
+        last_name: "Biden",
+    },
+    {
+        first_name: "Bill",
+        last_name: "Clitton",
+    },
+    {
+        first_name: "Hillary",
+        last_name: "Clinton",
+    },
+    {
+        first_name: "Bernie",
+        last_name: "Sanders",
+    },
+    {
+        first_name: "Barack",
+        last_name: "Obama",
+    }
+])
